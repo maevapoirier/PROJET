295f6f727b8e9e2ec9ac2025861a33436f617135
Gabs <Gabs@host.docker.internal>
2022-05-24 13:29:17 +0200

    Merge branch 'main' of https://github.com/maevapoirier/PROJET.git into main

--- src/main/java/fr/eni/projet/bo/ArticleVendu.java
+++ src/main/java/fr/eni/projet/bo/ArticleVendu.java
@@ -1,5 +1,101 @@
 package fr.eni.projet.bo;
 
-public class ArticleVendu {
+import java.util.Date;
 
+public class ArticleVendu {
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private Date dateDebutEncheres;
+	private Date dateFinEncheres;
+	private double miseAPrix; //prix de départ du produit
+	private double prixVente; //prix final de vente du produit
+	private boolean etatVente; //vente en cours=1, sinon=0
+	
+	private int utilisateur;
+	private Retrait retrait;
+	private Categorie categorie;
+	private Enchere enchere;
+	public int getNoArticle() {
+		return noArticle;
+	}
+	
+	
+	//constructeurs
+	public ArticleVendu(int noArticle, String nomArticle, String description, Date dateDebutEncheres,
+			Date dateFinEncheres, double miseAPrix, double prixVente, boolean etatVente) {
+		super();
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+	}
+
+	public ArticleVendu() {
+		super();
+	}
+
+
+	//getter et setter
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+	public String getNomArticle() {
+		return nomArticle;
+	}
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+	public void setDateDebutEncheres(Date dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+	public Date getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+	public void setDateFinEncheres(Date dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+	public double getMiseAPrix() {
+		return miseAPrix;
+	}
+	public void setMiseAPrix(double miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+	public double getPrixVente() {
+		return prixVente;
+	}
+	public void setPrixVente(double prixVente) {
+		this.prixVente = prixVente;
+	}
+	public boolean isEtatVente() {
+		return etatVente;
+	}
+	public void setEtatVente(boolean etatVente) {
+		this.etatVente = etatVente;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "ArticleVendu [noArticle=" + noArticle + ", nomArticle=" + nomArticle + ", description=" + description
+				+ ", dateDebutEncheres=" + dateDebutEncheres + ", dateFinEncheres=" + dateFinEncheres + ", miseAPrix="
+				+ miseAPrix + ", prixVente=" + prixVente + ", etatVente=" + etatVente + ", utilisateur=" + utilisateur
+				+ ", enchere=" + enchere + "]";
+	}
+	
+	
+	
 }
---
+++ src/main/java/fr/eni/projet/bo/Retrait.java
@@ -0,0 +1,9 @@
+package fr.eni.projet.bo;
+
+public class Retrait {
+	private String rue;
+	private int code_postal;
+	private String ville;
+	
+	private ArrayList<int> articleVendu;
+}
