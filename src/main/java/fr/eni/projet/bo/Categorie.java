e7aa1e459931bd198c0b7e0e13351697bd739979
Gabs <Gabs@host.docker.internal>
2022-05-24 10:11:25 +0200

    BO Categorie

---
+++ src/main/java/fr/eni/projet/bo/Categorie.java
@@ -0,0 +1,42 @@
+package fr.eni.projet.bo;
+
+public class Categorie {
+	private int noCategorie;
+	private String libelle;
+	
+	//constructeur
+	public Categorie(int noCategorie, String libelle) {
+		super();
+		this.noCategorie = noCategorie;
+		this.libelle = libelle;
+	}
+
+	public Categorie() {
+		super();
+	}
+
+	//getter et setter
+	public int getNoCategorie() {
+		return noCategorie;
+	}
+
+	public void setNoCategorie(int noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "Categorie [noCategorie=" + noCategorie + ", libelle=" + libelle + "]";
+	}
+	
+	
+}
