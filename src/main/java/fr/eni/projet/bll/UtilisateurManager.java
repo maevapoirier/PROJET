40e8a87e0ddd385919b010fecf605aae7fa74605
veyra <maeva.poirier2022@campus-eni.fr>
2022-06-02 12:09:08 +0200

    profil

--- src/main/java/fr/eni/projet/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/projet/bll/UtilisateurManager.java
@@ -114,6 +114,92 @@ public class UtilisateurManager {
 			return utilisateur;
 			
 		}
+		
+		public Utilisateur modifierUtilisateur(String pseudo,String nom,String prenom,String email,String telephone,String rue,String codePostal,String ville,String mdp, String mdp2) throws BusinessException 
+		{
+			
+			
+			System.out.println(pseudo);
+			BusinessException businessException = new BusinessException();
+			
+			// validation des données email dispo et pseudo dispo 
+			this.validerEmail(email, businessException);
+			this.validerPseudo(pseudo, businessException);
+			
+			if(nom.isEmpty())
+			{
+				businessException.ajouterErreur(CodesResultatBLL.NOM_PAS_RENSEIGNE);
+			}
+			if(prenom.isEmpty())
+			{
+				businessException.ajouterErreur(CodesResultatBLL.PRENOM_PAS_RENSEIGNE);
+			}
+			if(pseudo.isEmpty())
+			{
+				businessException.ajouterErreur(CodesResultatBLL.PSEUDO_PAS_RENSEIGNE);
+			}
+			if(email.isEmpty())
+			{
+				businessException.ajouterErreur(CodesResultatBLL.EMAIL_PAS_RENSEIGNE);
+			}
+			if(rue.isEmpty())
+			{
+				businessException.ajouterErreur(CodesResultatBLL.RUE_PAS_RENSEIGNEE);
+			}
+			if(codePostal.isEmpty())
+			{
+				businessException.ajouterErreur(CodesResultatBLL.CP_PAS_RENSEIGNE);
+			}
+			if(ville.isEmpty())
+			{
+				businessException.ajouterErreur(CodesResultatBLL.VILLE_PAS_RENSEIGNEE);
+			}
+			if(mdp.isEmpty())
+			{
+				businessException.ajouterErreur(CodesResultatBLL.MDP_PAS_RENSEIGNE);
+			}
+			if(mdp2.isEmpty())
+			{
+				businessException.ajouterErreur(CodesResultatBLL.MDP2_PAS_RENSEIGNE);
+			}
+			if(!mdp.equals(mdp2))
+			{
+				System.out.println("les mots de passe sont différents");
+				businessException.ajouterErreur(CodesResultatBLL.MDP_DIFFERENTS);
+			}
+			
+			
+			Utilisateur utilisateur = null;
+			System.out.println("je passe par l'utilisateur manager");
+			
+			if(!businessException.hasErreurs())
+			{
+				System.out.println("pas d'erreur jusque là");
+				utilisateur = new Utilisateur();
+				utilisateur.setPseudo(pseudo);
+				utilisateur.setNom(nom);
+				utilisateur.setPrenom(prenom);
+				utilisateur.setEmail(email);
+				utilisateur.setTelephone(telephone);
+				utilisateur.setRue(rue);
+				utilisateur.setCodePostal(codePostal);
+				utilisateur.setVille(ville);
+				utilisateur.setMotDePasse(mdp);
+				utilisateur.setCredit(credit);
+				utilisateur.setAdministrateur(administrateur);
+			
+				System.out.println(utilisateur);
+				this.utilisateurDAO.insert(utilisateur);
+			}
+			else
+			{	
+				System.out.println("business exception");
+				throw businessException;
+			}
+			System.out.println("RETURN " + utilisateur);
+			return utilisateur;
+			
+		}
 
 		private void validerEmail(String email, BusinessException businessException) {
 			if(utilisateurDAO.findByEmail(email) != null)
